e2ca1166a77ebe8ad750b934d886c993af83c9a2
Marko <marko.saraba@gmail.com>
2022-03-30 19:33:11 +0200

    druga vjezba

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+//jednolinijski komentar
+
+/* više
+linijski
+komentar 
+   */
+    // Only change code above this line
+}
+console.log(main());
+module.exports = main;
